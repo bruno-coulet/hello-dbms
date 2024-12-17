43d9ac9abc7319f9403f37e1b0c413f736b28458
ilyes-chabab <ilyes.chabab@laplateforme.io>
2024-12-17 12:35:35 +0100

    reparation du fichier csv + job 1 à 3 fait

---
+++ job3.sql
@@ -0,0 +1,18 @@
+--afficher toutes les colonnes de la table 
+select *
+from students
+
+--afficher les eleves de moins de 20 ans 
+select *
+from students
+where age < 20
+
+--le classement des meilleurs notes en ordre ascendant 
+select *
+from students
+order by grade asc
+
+--le classement des meilleurs notes en ordre descendant 
+select *
+from students
+order by grade desc 
