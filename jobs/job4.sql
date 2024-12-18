298f078b192baa6a3593821761ee5f27b5553603
bruno-coulet-data <bruno.coulet.pro@gmail.com>
2024-12-18 10:43:27 +0100

    révison de la fin du job4

--- jobs/job4.sql
+++ jobs/job4.sql
@@ -1,26 +1,35 @@
--- Active: 1719231795972@@127.0.0.1@3306@nobels
+-- Active: 1719231795972@@127.0.0.1@3306@world
 
+
+-- 1. Créez une requête permettant d’afficher les prix nobels de 1986.
 SELECT winner, subject
 FROM nobels
 WHERE yr = 1986
 
+
+-- 2. Créez une requête permettant d’afficher les prix nobels de littérature de 1967.
 SELECT winner
 FROM nobels
 WHERE yr = 1967 AND subject = 'Literature'
 
+--  3. Créez une requête permettant d’afficher l’année et le sujet du prix nobel d’Albert Einstein.
 SELECT yr, subject
 FROM nobels
 WHERE winner = 'Albert Einstein'
 
-
+-- 4. Créez une requête permettant d’afficher les détails (année, sujet,lauréat) des lauréats du prix de Littérature de 1980 à 1989 inclus.
 SELECT *
 FROM nobels
 WHERE yr > 1979 AND yr < 1990
 
+
+-- 5. Créez une requête permettant d’afficher les détails des lauréats du prix de Chimie.
 SELECT *
 FROM nobels
 WHERE subject = "Chemistry"
 
-COUNT row
+--  Combien y en a-t-il ? 
+SELECT COUNT(*) AS laureates_count
 FROM nobels
-WHERE subject = "Chemistry"
+WHERE subject = 'Chemistry';
+
