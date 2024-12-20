4fc54782aa4e2c4b1634832473102cd4dd39b4de
bruno-coulet-data <bruno.coulet.pro@gmail.com>
2024-12-20 14:09:13 +0100

    renommage de la table countries -> world et modification des requêtes en conséquence

--- job1.sql
+++ job1.sql
@@ -1,16 +1,23 @@
+show databases;
+use world;
+show tables;
+
+describe world;
+
+
+
+
 /*donnée de l'allemagne*/
 select *
 from world
-where name = 'Germany';
+where Country = 'Germany';
 
 /*données de suede , norvege et danemark*/
 select *
 from world
-where name in ('Sweden','Norway','Denmark');
+where Country in ('Sweden','Norway','Denmark');
 
 /*pays dont la superficie est entre 200k et 300k*/
-
-select *
+select Country, `Area (sq. mi.)`
 from world
-where `Area
-(sq. mi.)` between 200000 and 300000;
+where `Area (sq. mi.)` between 200000 and 300000;
