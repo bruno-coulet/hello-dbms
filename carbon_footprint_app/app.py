8ce249b3cee40f5e6cd1fea8a4d76a55a4891da3
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2025-01-21 10:58:00 +0100

    update

--- carbon_footprint_app/app.py
+++ carbon_footprint_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import mysql.connector
 from mysql.connector import Error
 import pandas as pd
@@ -31,6 +31,18 @@ def create_connection():
 def index():
     return render_template('index.html')
 
+
+@app.route('/countries')
+def get_countries():
+    connection = create_connection()
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT DISTINCT country FROM country;")
+        countries = [row[0] for row in cursor.fetchall()]
+    connection.close()
+    return countries
+
+
+
 @app.route('/total-emissions')
 def total_emissions():
     connection = create_connection()
@@ -41,8 +53,7 @@ def total_emissions():
                 coal_emissions + gas_emissions + oil_emissions + hydro_emissions + renewable_emissions + nuclear_emissions
             ) AS total_emissions
         FROM country
-        ORDER BY total_emissions DESC
-        LIMIT 20;
+        ORDER BY total_emissions DESC;
         """)
         results = cursor.fetchall()
     connection.close()
@@ -66,22 +77,43 @@ def coal_usage():
 
 @app.route('/energy-source-proportions')
 def energy_source_proportions():
+    country = request.args.get('country')
     connection = create_connection()
     with connection.cursor() as cursor:
-        cursor.execute("""
-        SELECT 
-            SUM(coal_emissions) AS total_coal,
-            SUM(gas_emissions) AS total_gas,
-            SUM(oil_emissions) AS total_oil,
-            SUM(hydro_emissions) AS total_hydro,
-            SUM(renewable_emissions) AS total_renewables,
-            SUM(nuclear_emissions) AS total_nuclear
-        FROM country;
-        """)
+        if country:
+            cursor.execute("""
+            SELECT 
+                SUM(coal_emissions) AS total_coal,
+                SUM(gas_emissions) AS total_gas,
+                SUM(oil_emissions) AS total_oil,
+                SUM(hydro_emissions) AS total_hydro,
+                SUM(renewable_emissions) AS total_renewables,
+                SUM(nuclear_emissions) AS total_nuclear
+            FROM country
+            WHERE country = %s;
+            """, (country,))
+        else:
+            cursor.execute("""
+            SELECT 
+                SUM(coal_emissions) AS total_coal,
+                SUM(gas_emissions) AS total_gas,
+                SUM(oil_emissions) AS total_oil,
+                SUM(hydro_emissions) AS total_hydro,
+                SUM(renewable_emissions) AS total_renewables,
+                SUM(nuclear_emissions) AS total_nuclear
+            FROM country;
+            """)
         results = cursor.fetchone()
     connection.close()
+    
+    if not results or all(v is None for v in results):
+        results = (0, 0, 0, 0, 0, 0)
+
     data = dict(zip(['total_coal', 'total_gas', 'total_oil', 'total_hydro', 'total_renewables', 'total_nuclear'], results))
-    return render_template('energy_proportions.html', data=data)
+    countries = get_countries()
+
+    
+    return render_template('energy_proportions.html', data=data, country=country, countries=countries)
 
 if __name__ == '__main__':
     app.run(debug=True)
