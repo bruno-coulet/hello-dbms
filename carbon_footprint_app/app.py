8d1d632023e571740732ce923eddf8ded6fc6f81
bruno-coulet-data <bruno.coulet.pro@gmail.com>
2025-01-24 16:39:06 +0100

    récupération à la main de app.py base.html

--- carbon_footprint_app/app.py
+++ carbon_footprint_app/app.py
@@ -1,6 +1,8 @@
+
 from flask import Flask, render_template, request
 import mysql.connector
 from mysql.connector import Error
+import plotly.express as px
 import pandas as pd
 import logging
 import config
